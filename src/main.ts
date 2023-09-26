407a0277884661d3e8ecbfde5c1569802c8d060e
dianabreik <diana.sam341@gmail.com>
2023-09-26 10:53:57 +0200

    double numbers in the list

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+const arrayOfNumbers: number[] = [1,2,3,4,5]
+
+const newArrayAfterMapFunction = arrayOfNumbers.map((number)=>
+{return  number*2})
+console.log(arrayOfNumbers)
+console.log(newArrayAfterMapFunction)
