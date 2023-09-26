739b3e5d399092814d4fd9695e081e3fbf5b64d5
dianabreik <diana.sam341@gmail.com>
2023-09-26 11:43:45 +0200

    use the (filter) function to get list for words with more than 5 letters + use the (reduce) function to get the sum of the numbers of an array

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,13 @@ const newArrayAfterMapFunction = arrayOfNumbers.map((number)=>
 {return  number*2})
 console.log(arrayOfNumbers)
 console.log(newArrayAfterMapFunction)
+
+const arrayOfWords: string[]= ["Hello","Hallo","Merhaba","Bonjour"]
+const filteredArray = arrayOfWords.filter((word): boolean =>{return  word.length > 5})
+console.log(arrayOfWords)
+console.log(filteredArray)
+
+
+const arrayOfNumbers2: number[] = [1,4,5]
+const sum = arrayOfNumbers2.reduce((accumulator,currentValue)=>{return accumulator + currentValue},0)
+console.log(sum)
