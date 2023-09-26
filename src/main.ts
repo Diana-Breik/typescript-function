bf0fb035ab55f2585e7ccac015753dda83255d3d
dianabreik <diana.sam341@gmail.com>
2023-09-26 12:00:36 +0200

    use the (some) function to find out if there is an element greater than 10 in the array

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,13 @@ console.log(filteredArray)
 const arrayOfNumbers2: number[] = [1,4,5]
 const sum = arrayOfNumbers2.reduce((accumulator,currentValue)=>{return accumulator + currentValue},0)
 console.log(sum)
+
+console.log("--------")
+
+const arrayOfNumbers3: number[] = [1,2,13]
+const result = arrayOfNumbers3.some((number)  =>{return number > 10})
+if (result) {
+    console.log("Mindestens eine Zahl im Array ist größer als 10.")
+} else {
+    console.log("Keine Zahl im Array ist größer als 10.")
+}
